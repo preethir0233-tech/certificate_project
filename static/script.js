908174ee65c06ce182f1af3dd2567400aee81c3e
preethir0233-tech <preethir0233@gmail.com>
2026-03-21 22:44:15 +0530

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,16 @@
+let time = 30;
+
+function startTimer() {
+    let timerDisplay = document.getElementById("timer");
+
+    let countdown = setInterval(function () {
+        time--;
+        timerDisplay.innerText = "Time: " + time + "s";
+
+        if (time <= 0) {
+            clearInterval(countdown);
+            alert("Time's up! Auto submitting...");
+            document.querySelector("form").submit();
+        }
+    }, 1000);
+}
