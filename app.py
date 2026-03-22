373f43bd0ea5efb0ff793ec91cf2834c71bb604f
preethi <preethi0233@gmail.com>
2026-03-22 08:04:24 +0530

    added procfile for deployment

--- app.py
+++ app.py
@@ -210,4 +210,4 @@ def add_border(canvas, doc):
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=5000)
